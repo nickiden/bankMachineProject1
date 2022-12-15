9eb5c53e181005a5a29faca2038febd97e517eda
nikkifixx <iden.nick@gmail.com>
2022-12-14 21:43:30 -0500

    Initial Commit

---
+++ BankMachineProject1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BankMachineProject1
+//
+//  Created by Nick Iden on 2022-12-14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
