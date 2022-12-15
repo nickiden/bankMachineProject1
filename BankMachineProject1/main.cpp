c9bc6b4698de34976c995910b469f0827a6dcba6
nikkifixx <iden.nick@gmail.com>
2022-12-14 22:08:34 -0500

    initial commit of the project

--- BankMachineProject1/main.cpp
+++ BankMachineProject1/main.cpp
@@ -1,14 +1,33 @@
-//
-//  main.cpp
-//  BankMachineProject1
-//
-//  Created by Nick Iden on 2022-12-14.
-//
+// PROJECT  : BackMachineProject#1
+// FILE     : main.cpp
+// DATE     : Dec 15th, 2002
+// PRPGRAMMER   :   Nick Iden
+// COMMENT  :
+//              This is a simple application that displays a menu, accept user input, and
+//              verify the data entered is acceptable.
+//              Steps:
+//                      1. Create a simple menu function
+//                      2. Allow user to add funds
+//                      3. Allow user to withdrawl funds
+// NOTE : later versions will include user log in and log out, remembering user acount info, and transfers.
+
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(void)
+{
+    
     return 0;
+}
+
+
+// Function : displayMenu
+// date :   December 15th 2022
+// comment : Menu displayed for user.
+
+
+void displayMenu()
+{
+    
+    
 }
